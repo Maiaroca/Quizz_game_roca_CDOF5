cb0a7d3dec64e3d6f37a9fe4719faab9db564230
Kerrouche Rayane <rayane.kerrouche@hotmail.com>
2025-01-14 15:24:52 +0100

    J'ai rajouté une question

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,8 @@ questions = [
     {"question": "Quelle est la capitale de la France ?", "answer": "Paris"},
     {"question": "Combien font 2 + 2 ?", "answer": "4"},
     {"question": "Quel est le langage de programmation utilisé ici ?", "answer": "Python"},
+    {"question": "En quelle année est née Maia ?", "answer": "2005"},
+
 ]
 
 score = 0
