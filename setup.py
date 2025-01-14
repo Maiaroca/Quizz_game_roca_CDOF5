e6e59593b6b94c26b0e6bba12f0f431a4e2ed50c
Maiaroca <146460693+Maiaroca@users.noreply.github.com>
2025-01-14 14:10:34 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='{Quizz_game}',
+  version='1.0',
+  author='{Maiaroca}',
+  license='MIT',
+  long_description=open('README.md').read(),
+)
